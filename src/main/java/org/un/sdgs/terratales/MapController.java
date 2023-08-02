5446801c358e9ab83183afd992872ccd378e599c
Alawnely <15603444+alawnely@users.noreply.github.com>
2023-08-02 21:55:02 +0800

    Added currentUser to map view

--- src/main/java/org/un/sdgs/terratales/MapController.java
+++ src/main/java/org/un/sdgs/terratales/MapController.java
@@ -1,39 +1,28 @@
 package org.un.sdgs.terratales;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class MapController extends Application {
+public class MapController {
     @FXML
     private ImageView mapImage;
+    @FXML
+    private Label userLabel;
     private Image map;
     private int mapX, mapY, mapZoomLevel;
 
-    public static void main(String[] args) {
-        launch();
-    }
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("map-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
-    }
-
     @FXML
     private void initialize() {
+        userLabel.setText(UserDatabase.currentUser.getUsername());
+
         map = new Image(Objects.requireNonNull(getClass().getResource("img/maplightmodegreen_expanded.png")).toExternalForm());
         mapX = 0;
         mapY = 0;
