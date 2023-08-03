0bd7478a086f113430c84ea77b4e62f9fd8181df
Alawnely <15603444+alawnely@users.noreply.github.com>
2023-08-03 19:15:43 +0800

    Left-aligned location buttons

--- src/main/java/org/un/sdgs/terratales/FavoritesController.java
+++ src/main/java/org/un/sdgs/terratales/FavoritesController.java
@@ -2,9 +2,9 @@ package org.un.sdgs.terratales;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -57,6 +57,7 @@ public class FavoritesController {
 
         Button button = new Button(locName, imageView);
         button.setMaxWidth(Double.MAX_VALUE);
+        button.setAlignment(Pos.CENTER_LEFT);
         button.setOnAction(mouseEvent -> changeLocation(locName));
         return button;
     }
